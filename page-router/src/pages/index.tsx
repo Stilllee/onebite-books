dca336655015d0c00daebd13a6627eb51d8afb9a
Stilllee <orodae@gmail.com>
2024-08-23 00:13:24 +0900

    [PAGE ROUTER] SSR 1 #5

--- page-router/src/pages/index.tsx
+++ page-router/src/pages/index.tsx
@@ -1,10 +1,32 @@
+import { ReactNode, useEffect } from "react";
+
 import BookItem from "@/components/book-item";
-import { ReactNode } from "react";
+import { InferGetServerSidePropsType } from "next";
 import SearchableLayout from "@/components/searchable-layout";
 import books from "@/mock/books.json";
 import styles from "./index.module.css";
 
-export default function Home() {
+export const getServerSideProps = () => {
+  // 서버에서만 실행됨
+  const data = "hello";
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+export default function Home({
+  data,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  console.log(data); // 서버에서 한 번, 클라이언트에서 한 번 출력됨
+
+  useEffect(() => {
+    // 마운트 후 클라이언트에서만 실행됨
+    console.log(window);
+  }, []);
+
   return (
     <div className={styles.container}>
       <section>
