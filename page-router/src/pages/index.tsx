70e2171ede1c39931367cfae862e52cccf501247
Stilllee <orodae@gmail.com>
2024-08-23 01:01:08 +0900

    [PAGE ROUTER] SSR 2 #5

--- page-router/src/pages/index.tsx
+++ page-router/src/pages/index.tsx
@@ -1,43 +1,41 @@
-import { ReactNode, useEffect } from "react";
-
 import BookItem from "@/components/book-item";
 import { InferGetServerSidePropsType } from "next";
+import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 import styles from "./index.module.css";
 
-export const getServerSideProps = () => {
-  // 서버에서만 실행됨
-  const data = "hello";
+export const getServerSideProps = async () => {
+  // api 호출을 동시에 하기 위해 Promise.all을 사용
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
     props: {
-      data,
+      allBooks,
+      recoBooks,
     },
   };
 };
 
 export default function Home({
-  data,
+  allBooks,
+  recoBooks,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  console.log(data); // 서버에서 한 번, 클라이언트에서 한 번 출력됨
-
-  useEffect(() => {
-    // 마운트 후 클라이언트에서만 실행됨
-    console.log(window);
-  }, []);
-
   return (
     <div className={styles.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
--- page-router/src/pages/search/index.tsx
+++ page-router/src/pages/search/index.tsx
@@ -1,13 +1,26 @@
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+
 import BookItem from "@/components/book-item";
 import { ReactNode } from "react";
 import SearchableLayout from "@/components/searchable-layout";
-import books from "@/mock/books.json";
-import { useRouter } from "next/router";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
-  const router = useRouter();
-  const { q } = router.query;
+export const getServerSideProps = async (
+  context: GetServerSidePropsContext
+) => {
+  const { q } = context.query;
+  const books = await fetchBooks(q as string);
 
+  return {
+    props: {
+      books,
+    },
+  };
+};
+
+export default function Page({
+  books,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div>
       {books.map((book) => (
