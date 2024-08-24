6e40021e1d2ca3cf0353c814508f92f43720bf0c
Stilllee <orodae@gmail.com>
2024-08-24 22:50:24 +0900

    [PAGE ROUTER] SEO 설정 #6

--- page-router/src/pages/book/[id].tsx
+++ page-router/src/pages/book/[id].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 
+import Head from "next/head";
 import fetchOneBook from "@/lib/fetch-one-book";
 import styles from "./[id].module.css";
 import { useRouter } from "next/router";
@@ -37,25 +38,48 @@ export default function Page({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   const router = useRouter();
 
-  if (router.isFallback) return "로딩중입니다.";
+  if (router.isFallback) {
+    return (
+      <>
+        <Head>
+          <title>한입북스</title>
+          <meta property="og:image" content="/thumbnail.png" />
+          <meta property="og:title" content="한입북스" />
+          <meta
+            property="og:description"
+            content="한입 북스에 등록된 도서들을 만나보세요."
+          />
+        </Head>
+        <div>로딩중입니다.</div>
+      </>
+    );
+  }
   if (!book) return "문제가 발생했습니다. 다시 시도하세요.";
 
   const { title, subTitle, description, author, publisher, coverImgUrl } = book;
 
   return (
-    <div className={styles.container}>
-      <div
-        className={styles.cover_img_container}
-        style={{ backgroundImage: `url('${coverImgUrl}')` }}
-      >
-        <img src={coverImgUrl} />
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:image" content={coverImgUrl} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+      </Head>
+      <div className={styles.container}>
+        <div
+          className={styles.cover_img_container}
+          style={{ backgroundImage: `url('${coverImgUrl}')` }}
+        >
+          <img src={coverImgUrl} />
+        </div>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.subTitle}>{subTitle}</div>
+        <div className={styles.author}>
+          {author} | {publisher}
+        </div>
+        <div className={styles.description}>{description}</div>
       </div>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.subTitle}>{subTitle}</div>
-      <div className={styles.author}>
-        {author} | {publisher}
-      </div>
-      <div className={styles.description}>{description}</div>
-    </div>
+    </>
   );
 }
