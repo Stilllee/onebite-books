77b83a56e60aafcdc317e890b123239f16f96718
Stilllee <orodae@gmail.com>
2024-08-30 22:28:31 +0900

    [APP ROUTER] 스트리밍 2 - 컴포넌트 스트리밍 #14

--- app-router/src/app/(with-searchbar)/search/page.tsx
+++ app-router/src/app/(with-searchbar)/search/page.tsx
@@ -1,20 +1,15 @@
 import { BookData } from "@/types";
 import BookItem from "@/components/book-item";
+import { Suspense } from "react";
 import { delay } from "@/util/delay";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: {
-    q?: string;
-  };
-}) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
 
   // 검색 결과를 동적으로 가져오는 동적 페이지이기 때문에 풀 라우트 캐싱은 불가능
   // 데이터 캐시를 활용한 최적화만 가능
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`,
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
   );
   if (!response.ok) {
@@ -29,5 +24,19 @@ export default async function Page({
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+export default function Page({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<div>로딩중...</div>}>
+      <SearchResult q={searchParams.q || ""} />
+    </Suspense>
   );
 }
