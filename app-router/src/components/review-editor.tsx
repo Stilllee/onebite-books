14c19061cf7114accb3cb7b84958a999914be1d8
Stilllee <orodae@gmail.com>
2024-09-15 19:13:28 +0900

    [APP ROUTER] 서버액션 - 클라이언트 컴포넌트 #18

--- app-router/src/components/review-editor.tsx
+++ app-router/src/components/review-editor.tsx
@@ -1,16 +1,30 @@
+"use client";
+
+import { useActionState, useEffect } from "react";
+
 import { createReviewAction } from "@/actions/create-review.action";
 import styles from "./review-editor.module.css";
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) alert(state.error);
+  }, [state]);
+
   return (
     <section>
-      <form className={styles.form_container} action={createReviewAction}>
+      <form className={styles.form_container} action={formAction}>
         <input name="bookId" value={bookId} hidden readOnly />
         <textarea
           required
           autoComplete="off"
           name="content"
           placeholder="리뷰 내용"
+          disabled={isPending}
         />
         <div className={styles.submit_container}>
           <input
@@ -18,8 +32,11 @@ export default function ReviewEditor({ bookId }: { bookId: string }) {
             autoComplete="off"
             name="author"
             placeholder="작성자"
+            disabled={isPending}
           />
-          <button type="submit">작성하기</button>
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
